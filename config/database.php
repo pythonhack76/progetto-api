cf35857eebb56e1212b782f6a6801d3d3ba37095
Python The Hack <lrulvoni@gmail.com>
2021-10-07 15:09:17 +0200

    first commit

---
+++ config/database.php
@@ -0,0 +1,33 @@
+<?php 
+
+
+class Database{
+
+    //variabili 
+    private $hostname;
+    private $dbname;
+    private $username;
+    private $pasword;
+    private $conn; 
+
+    public function connect(){
+        //inizializzazione delle variabili 
+        $this->hostname = "localhost";
+        $this->dbname = "rest_php_api";
+        $this->username = "root";
+        $this->password = "root";
+
+        $this->conn = new mysqli($this->hostname, $this->username, $this->password);
+        if($this->conn->connect_errno){
+                print_r($this->conn->connect_error);
+                exit;
+        }else{
+            return $this->conn; 
+        }
+        
+    }
+
+}
+
+
+?>
